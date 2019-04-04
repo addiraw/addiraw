aae06dc342874f4b87fbde6f417603cac9adf429
aditya kumar <adityakumar1995rawat@gmail.com>
2019-04-04 23:12:17 +0530

    latest

--- javascript.js
+++ javascript.js
@@ -48,3 +48,21 @@ function showSlides(n)
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+
+
+       $(window).on('scroll',function()
+
+       {
+           if($(window).scrollTop()){
+               $('nav').addClass('black');
+
+           }
+           else{
+               $('nav').removeClass('black');
+           }
+
+       })
+       
+     
+    
